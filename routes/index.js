09fee0dc856f132ca5bc93a417bd54a17d2aa005
Bethany Priode <bpriode@gmail.com>
2017-08-29 16:43:51 -0400

    edit, delete, create all working

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const Book    = require('../models/book');
 const router  = express.Router();
 
+let data = [];
+
+
+let getData = function (db, callback) {
+  let users = db.collection('books');
+
+    User.find({}).toArray().sort({'title': 1}).then(function(books) {
+        data = books;
+        callback();
+    });
+};
 
 router.get('/', function(req, res) {
 
   Book.find({})
   .then(function(books) {
     res.render('listing', {bookData: books});
+  })
+  .catch(function(err) {
+    res.send(err);
   })
 });
 
@@ -27,19 +41,16 @@ router.post('/', function(req, res) {
     Book.create(newBook)
     .then(function(data){
       res.redirect('/');
-        // console.log(data);
     })
     .catch(function(err){
-        // console.log(err);
+      res.send(err);
     })
-    // res.redirect('/');
-  })
+  });
 
 
-router.post('/delete/:id', function(req, res) {
+router.get('/delete/:id', function(req, res) {
 
-  let id = req.body.deleteBtn;
-  // console.log(title);
+  let id = req.params.id;
 
   Book.deleteOne({_id: id})
   .then(function (data) {
@@ -47,51 +58,52 @@ router.post('/delete/:id', function(req, res) {
   })
   .catch(function(err) {
     res.send(err);
-  });
-});
-//
-//
-router.get('/edit/:id', function(req, res){
-  let editId = req.params.id;
-  let bookEdit = data.find(function(book) {
-    return book.id = editId;
   })
-  // Book.find({name: name})
-  // .then(function(book){
-    res.render('edit', {bookData: bookEdit})
-  });
+});
+
+// router.get('/edit/:id', function(req, res){
+//   let editId = req.params.id;
+//   let bookEdit = data.find(function(book) {
+//     return book.id == editId
+//   })
+//     res.render('edit', {books: bookEdit})
+//     // console.log(books);
+//   // })
+//   //   .catch(function(err) {
+//   //     res.send(err);
+//   //   })
+//   });
+
+  router.get('/edit/:id' ,function (req, res) {
+
+  Book.find({_id: req.params.id})
+  .then(function(books) {
+  res.render('edit', {books: books});
+})
+  .catch(function(err) {
+    res.send(err);
+  })
+});
 
 router.post('/edit/:id', function(req, res) {
+  let bookEdit = req.params.id
 
-  let editId = req.params.id;
-
-
-  Book.update({_id: userEdit}, {
+  Book.update({_id: bookEdit}, {
       title: req.body.title,
       author: req.body.author,
-      pageCount: req.body.page,
+      pageCount: req.body.page || null,
       publishing: {
-        year: req.body.year,
-        publisher: req.body.publisher,
-        city: req.body.city
+        year: req.body.year || null,
+        publisher: req.body.publisher || null,
+        city: req.body.city || null
       }
-    }).then(function(data) {
+    }).then(function(book) {
       res.redirect('/');
     })
     .catch(function(err) {
       res.send(err);
     })
 })
-
-
-
-
-
-// router.post('/:id', function (req, res) {
-//   let id = req.params.id;
-//   res.redirect('/');
-// })
-
 
 
 module.exports = router
